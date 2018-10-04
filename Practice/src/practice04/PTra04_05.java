7880ccbf0216d33c3c19f7576505236b04a9066e
DESKTOP-TJMIP1U\User <saori.harada.likeit@gmail.com>
2018-10-04 14:40:54 +0900

    4-1～5-4

--- Practice/src/practice04/PTra04_05.java
+++ Practice/src/practice04/PTra04_05.java
@@ -44,6 +44,8 @@ public class PTra04_05 {
 			 *
 			 * ※ プログラムは何行書いても良いです
 			 */
+
+
 			if (cpuHand == 0) {
 				System.out.println("グー！");
 
@@ -57,10 +59,27 @@ public class PTra04_05 {
 			} else {
 				System.out.println("パー！！");
 
-
-
 			}
 
+			if(myHand == 0 && cpuHand ==0){
+				continue;
+			}else if(myHand == 0 && cpuHand == 1){
+				break;
+			}else if(myHand == 0 && cpuHand == 2){
+				continue;
+			}else if(myHand == 1 && cpuHand == 1){
+				continue;
+			}else if(myHand == 1 && cpuHand == 2){
+				break;
+			}else if(myHand == 1 && cpuHand == 0){
+				continue;
+			}else if(myHand == 2 && cpuHand == 2){
+				continue;
+			}else if(myHand == 2 && cpuHand == 0){
+				break;
+			}else if(myHand == 2 && cpuHand == 1){
+				break;
+			}
 		}
 	}
 }
