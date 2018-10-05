30a1e0073680ba50cc83bc53b4e27e550d64f975
DESKTOP-TJMIP1U\User <saori.harada.likeit@gmail.com>
2018-10-05 18:43:25 +0900

    10-1～10-4

--- Practice/src/practice10/PTra10_03.java
+++ Practice/src/practice10/PTra10_03.java
@@ -1,4 +1,5 @@
 package practice10;
+
 /*
  * PTra10_03.java
  *   作成	LIKEIT	2017
@@ -16,27 +17,34 @@ public class PTra10_03 {
 
 		// Userクラスを作成後に着手してください
 		// ★ User型の変数usを宣言してください
+		User us;
 
 
 		// ★ 変数usに、Userクラスのインスタンスを作成・代入してください
-
+		us = new User();
 
 		System.out.println("ユーザID（数字）、 ユーザ名、メールアドレス、パスワードをカンマ区切りで入力してください");
 
 		// ★ java.util.Scannerクラスをインスタンス化し、変数scannerに代入してください
-
+		String line = new java.util.Scanner(System.in).nextLine();
 
 		// ★ 入力された値を、String型の変数lineに格納してください
 
 
 		// ★ 変数lineに格納されている値を','で区切り、配列にしてください（Stringクラスのメソッドを使用します）
-
+		String[] i = line.split(",");
 
 		// ★ 配列にした値を、usの各フィールドに代入してください
 		// ユーザIDに該当する値が数値でなかった場合は、エラーでプログラムが止まっても良いです
-
+		us.userId = Integer.parseInt(i[0]);
+		us.userNm = i[1];
+		us.mail = i[2];
+		us.password = i[3];
 
 		// ★ 変数usに格納されているインスタンスの、フィールドuserId, userNm, mail, passwordを出力してください
-
+		System.out.println(us.userId);
+		System.out.println(us.userNm);
+		System.out.println(us.mail);
+		System.out.println(us.password);
 	}
 }
