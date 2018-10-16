1e03c05b8fe660b53b73717cfec714612b37ce26
DESKTOP-TJMIP1U\User <saori.harada.likeit@gmail.com>
2018-10-16 11:11:39 +0900

    17-3,4

--- Practice/src/practice17/ptra17/PTra17_03.java
+++ Practice/src/practice17/ptra17/PTra17_03.java
@@ -40,24 +40,26 @@ public class PTra17_03 {
 				System.out.println("問題：" + (i + 1));
 				System.out.println(question[i]);
 
+			while(true) {
 				System.out.println("回答を数字で入力してください");
 				String input = ThrowExceptionUtil.inputValue();
 
+				try {
 				int num = Integer.parseInt(input);
 
 				if (answer[i] == num) {
 					score++;
 				}
+				break;
+				}catch(NumberFormatException e) {
+					System.out.println("数字以外が入力されました");
+				}
+			}
 			}
 		} catch(IOException e) {
 			System.out.println("例外が発生しました");
-		} catch(NumberFormatException e) {
-			System.out.println("数字以外が入力されました");
 		}
-
-		System.out.println("全ての問題が終わりました。");
-		System.out.println("あなたの得点は・・・" + score + "点です！");
-
+				System.out.println("全ての問題が終わりました。");
+				System.out.println("あなたの得点は・・・" + score + "点です！");
 	}
-
 }
